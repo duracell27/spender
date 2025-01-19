8ad3d1ce0487a66dbcf7e538cc2dce968b54973c
Duracell27 <volodymyrshmidt@gmail.com>
2025-01-20 00:00:03 +0200

    фікс 3

--- src/app/dashboard/transactions/[page]/page.tsx
+++ src/app/dashboard/transactions/[page]/page.tsx
@@ -6,15 +6,20 @@ import { ActiveWalletSelect } from "@/components/forms/UserDashboardWaletSelect"
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-type Params = Promise<{ page: string }>
+type Params = Promise<{ page: string, tab?: string }>
 
-const TransactionPage = async ({ params, searchParams }: { params: Params, searchParams: { tab?: string } }) => {
+
+const TransactionPage = async ({ params,
+  //  searchParams
+   }: { params: Params,
+    //  searchParams: searchParams 
+    }) => {
 
   console.log('params', params)
 
   const { page } = await params; // отримуємо номер сторінки для пагінації
   const pageNumber = parseInt(page, 10) || 1 // якщо не було передано номер сторінки пагінації то ставив першу
-  const searchParam = searchParams.tab; // отримуємо параметр з строки щоб визначити яку вкладку потрібно відобразити
+  const searchParam = (await params).tab; // отримуємо параметр з строки щоб визначити яку вкладку потрібно відобразити
 
   const session = await auth();
   if (!session?.user.id) return;
